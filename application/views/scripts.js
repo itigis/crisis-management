da83b48bd49ddfa2bbffd897be4728f24d281af7
MxMira <m.atef.mira@gmail.com>
2017-04-06 21:08:08 +0200

    Leaflet&Mapbox_Added

--- application/views/scripts.js
+++ application/views/scripts.js
@@ -1,24 +1,18 @@
 
 /* google maps -----------------------------------------------------*/
-function initialize() {
-
-  /* position Amsterdam */
-  var latlng = new google.maps.LatLng(30, 31);
-
-  var mapOptions = {
-    center: latlng,
-    scrollWheel: false,
-    zoom: 5
-  };
-  
-  var marker = new google.maps.Marker({
-    position: latlng,
-    url: '/',
-    animation: google.maps.Animation.DROP
-  });
-  
-  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-  marker.setMap(map);
-
-};
+(function initialize(){
+  var mymap = L.map('map-canvas').setView([27, 30], 6);
+  L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibS1taXJhIiwiYSI6ImNqMTZwOG5qeDAycXQycXFzZmNyczlsNzMifQ.ghxfG81qxR3ygxL8TrkL0w', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 15,
+      id: 'your.mapbox.project.id',
+      accessToken: 'your.mapbox.public.access.token'
+  }).addTo(mymap);
+  var marker = L.marker([31, 30]).addTo(mymap);
+  var polygon = L.polygon([
+      [27.5, 30.5],
+      [27.25, 30.25],
+      [27.0, 30.0]
+  ]).addTo(mymap);
+})()
 /* end google maps -----------------------------------------------------*/
