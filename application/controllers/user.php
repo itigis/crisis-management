fdea178abb95d5c83a6fa3246de7a1756fde35f1
Melegy <a.meligy@outlook.com>
2017-04-12 17:52:50 +0200

    user update

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -62,4 +62,38 @@ class User extends CI_Controller {
 }
 
 
+public function update($id = NULL)
+{
+        $data['user_item'] = $this->users->get_user_by_id($id);
+
+                if (empty($data['user_item']))
+        {
+                show_404();
+        }
+
+    $this->load->helper('form');
+    $this->load->library('form_validation');
+
+    $data['title'] = 'Manage user account';
+
+    $this->form_validation->set_rules('id', 'ID', 'required');
+    $this->form_validation->set_rules('name', 'Name', 'required');
+    $this->form_validation->set_rules('password', 'Password', 'required');
+    $this->form_validation->set_rules('email', 'E_mail', 'required');
+    
+
+    if ($this->form_validation->run() === FALSE)
+    {
+        $this->load->view('templates/header', $data);
+        $this->load->view('pages/admin/update');
+        $this->load->view('templates/footer');
+
+    }
+    else
+    {
+        $this->users->update_user($id);
+        $this->load->view('pages/admin/success');
+    }
+}
+
 }
